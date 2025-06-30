e212cec5bf8abea0c4fc031fd3c8548888122e69
Henry Wiechert <henry.wiechert@gmx.de>
2025-06-30 23:30:30 +0200

    grid layout prep

--- frontend/src/components/Visualization/charts/ScatterPlot.tsx
+++ frontend/src/components/Visualization/charts/ScatterPlot.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { CellSpec } from '../../../spec-generator/specGenerator';
 
-const ScatterPlot: React.FC = () => {
+interface ChartProps {
+  cellSpec: CellSpec;
+  // data: any[];
+}
+
+const ScatterPlot: React.FC<ChartProps> = ({ cellSpec }) => {
+  const { encoding } = cellSpec;
   return (
-    <div>
-      <p>ScatterPlot</p>
+    <div style={{ padding: '8px', border: '1p solid #eee', height: '100%' }}>
+      <p>Scatter Plot</p>
+      <p>
+        X: {encoding.x?.columnName || ' (none)'}
+      </p>
+      <p>
+        Y: {encoding.y?.columnName || ' (none)'}
+      </p>
     </div>
   );
 };
