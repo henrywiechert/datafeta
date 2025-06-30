3c3bf17dc76eaddc3f5c60751d1397a372c1104b
Henry Wiechert <henry.wiechert@gmx.de>
2025-06-30 23:30:30 +0200

    grid layout prep

--- frontend/src/components/Visualization/charts/BarChart.tsx
+++ frontend/src/components/Visualization/charts/BarChart.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { CellSpec } from '../../../spec-generator/specGenerator';
 
-const BarChart: React.FC = () => {
+interface ChartProps {
+  cellSpec: CellSpec;
+  // data: any[];
+}
+
+const BarChart: React.FC<ChartProps> = ({ cellSpec }) => {
+  const { encoding, orientation } = cellSpec;
   return (
-    <div>
-      <p>BarChart</p>
+    <div style={{ padding: '8px', border: '1px solid #eee', height: '100%' }}>
+      <p>Bar Chart ({orientation})</p>
+      <p>
+        X: {encoding.x?.columnName || ' (none)'}
+      </p>
+      <p>
+        Y: {encoding.y?.columnName || ' (none)'}
+      </p>
     </div>
   );
 };
