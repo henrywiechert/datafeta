995bed1b6ca55b3d61bc1b8ad7a11db61085f722
Henry Wiechert <henry.wiechert@gmx.de>
2025-08-09 17:43:56 +0200

    Simplify faceting

--- frontend/src/observable-plot-generator/layers/index.ts
+++
@@ -1,4 +0,0 @@
-// Export all faceting layers for easy importing
-export { SingleChartLayer } from './singleChartLayer';
-export { MultiMeasureLayer } from './multiMeasureLayer';
-export { DimensionFacetingLayer } from './dimensionFacetingLayer';
