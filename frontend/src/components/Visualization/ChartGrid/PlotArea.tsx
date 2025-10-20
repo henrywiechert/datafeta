9c690084f5e46f2b85c492129b158219d083a245
Henry Wiechert <henry.wiechert@nokia.com>
2025-10-20 18:27:00 +0200

    Revert "Fix x-scale offset issue for bottom chart."
    This reverts commit 1f6dcc8bfd80ddc899538a78de8de16e9a6778a3.

--- frontend/src/components/Visualization/ChartGrid/PlotArea.tsx
+++ frontend/src/components/Visualization/ChartGrid/PlotArea.tsx
@@ -21,7 +21,6 @@ function suppressAxes(options: any, hideX: boolean, hideY: boolean) {
   next.marginLeft = 0;
   next.marginRight = 0;
   next.marginTop = 0;
-  // Add small bottom margin to prevent X-axis overlap with chart marks
   next.marginBottom = 0;
   next.inset = 0;
   if (hideX) {
@@ -51,7 +50,7 @@ const PlotArea: React.FC<PlotAreaProps> = ({
   totalContentWidthPx,
 }) => {
   return (
-    <div style={{ gridColumn: 1, gridRow: spec.facetLabels ? 2 : 1}}>
+    <div style={{ gridColumn: 1, gridRow: spec.facetLabels ? 2 : 1, overflow: 'hidden', position: 'relative' }}>
       <div
         ref={plotsTranslateRef}
         style={{
