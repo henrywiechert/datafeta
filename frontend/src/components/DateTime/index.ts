e1a9f06ddf7d82773124b7b485720b782604ca6d
Henry Wiechert <henry.wiechert@gmx.de>
2025-11-10 17:01:47 +0100

    DateTime filter improvements

--- frontend/src/components/DateTime/index.ts
+++ frontend/src/components/DateTime/index.ts
@@ -5,5 +5,6 @@
  */
 
 export { default as DateTimeFilterControl } from './DateTimeFilterControl';
+export { default as DateTimeRangeFilter } from './DateTimeRangeFilter';
 export { default as DateTimePartMenu } from './DateTimePartMenu';
 
